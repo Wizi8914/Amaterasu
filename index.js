d9f6aeae5e8bb2c1ddc3863b19dee3292b4f5153
Wizi8914 <loick.konieczny@gmail.com>
2021-08-06 23:45:22 +0200

    remove test line

--- index.js
+++ index.js
@@ -171,34 +171,3 @@ client.on('error', (error) => console.error(error));
 
 
 client.login(process.env.DISCORD_TOKEN);
-
-
-
-client.on("message", async (message) => {
-    if (message.content.startsWith("{play")) {
-        // Note: This example only works for retrieving tracks using a query, such as "Rick Astley - Never Gonna Give You Up".
-    
-        // Retrieves tracks with your query and the requester of the tracks.
-        // Note: This retrieves tracks from youtube by default, to get from other sources you must enable them in application.yml and provide a link for the source.
-        // Note: If you want to "search" for tracks you must provide an object with a "query" property being the query to use, and "source" being one of "youtube", "soundcloud".
-        console.log('uwu')
-
-        const res = await client.manager.search(
-            message.content.slice(6),
-            message.author
-        );
-    
-        // Create a new player. This will return the player if it already exists.
-        const player = client.manager.create({
-            guild: message.guild.id,
-            voiceChannel: message.member.voice.channel.id,
-            textChannel: message.channel.id,
-            selfDeafen: false,
-        });
-
-        console.log(player)
-    
-        // Connect to the voice channel.
-        player.connect();
-    }
-})
