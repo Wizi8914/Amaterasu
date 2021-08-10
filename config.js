dad3d1445b76838a7275e5f87a1f0210e187fd7a
Wizi8914 <loick.konieczny@gmail.com>
2021-08-10 16:56:17 +0200

    remove badwords

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     botname: "Amaterasu",
     botimage: "https://i.ibb.co/Gdyc1j5/logo-koniko.png",
-    badwords: ["nigga", "nigger", "suck", "fuck", "clit", "porn", "pervert", "blowjob", "porn", "dick", "bitch", "horny", "penis", "vagina", "asshole", "racist", "shit", "🍆", "bastard", "piss", "cum", "🍌", "🍑", "cumming", "cummed", "cuming", "cumed", "sex", "blowjob", "tit", "boob", 
+    badwords: ["nigga", "nigger", "suck", "fuck", "clit", "porn", "pervert", "blowjob", "porn", "dick", "bitch", "horny", "penis", "vagina", "asshole", "racist", "shit", "🍆", "bastard", "piss", "cum", "🍌", "🍑", "cumming", "cummed", "cuming", "cumed", "sex", "blowjob", "boob", 
     "🖕", "gay", "lesbian", "pubic", "sperm", "hoe", "whore", "pussy", "orgasm", "fuk", "erotic", "hentai", "vibrator", "anal", "condom", "masturbate", "shag", "nude", "cunt", "milf", "squirt", "felch", "cock", "f u c k"]
 };
