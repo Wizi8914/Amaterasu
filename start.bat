36be6aaa116263ab2bb73e879f39e8eb537219e2
Wizi8914 <loick.konieczny@gmail.com>
2021-08-15 20:51:31 +0200

    idk

--- start.bat
+++ start.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 ECHO ==========================
-ECHO Starting Lavalink
+ECHO Lancement du Server Lavalink
 ECHO ==========================
 start cmd /k java -jar ./Lavalink.jar
 ECHO ==========================
-@ECHO Taking a 5 Second Break for Lavalink
+@ECHO Attender 5 seconds que lavalink ce lance
 ECHO ==========================
 timeout /T 5 /nobreak
 ECHO ==========================
-@ECHO Starting BOT
+@ECHO Lancement du Bot
 ECHO ==========================
 start cmd /k node index.js
 exit /s
