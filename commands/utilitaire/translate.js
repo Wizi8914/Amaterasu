d1432a6ff4d5cfcb3e97023d4e5627b70d5b5ea5
Wizi8914 <loick.konieczny@gmail.com>
2021-08-17 19:28:44 +0200

    idk

--- commands/utilitaire/translate.js
+++ commands/utilitaire/translate.js
@@ -1,6 +1,8 @@
 const { Command, CommandoMessage } = require('discord.js-commando');
-const translate = require('google-translate-api');
- 
+const translate = require('deepl')
+
+require('dotenv').config()
+
 module.exports = class TranslateCommand extends Command {
     constructor(client) {
         super(client, {
@@ -17,10 +19,22 @@ module.exports = class TranslateCommand extends Command {
      */
  
     async run(message, args) {
-        const query = args.split(' ')
+        
+        const str = args.split(' ')
 
-        translate(query, {to: 'en'}).then(res => {
-            console.log(res.text)
+
+        translate({
+            free_api: true,
+            text: args,
+            source_lang: str[0],
+            target_lang: str[1],
+            auth_key: process.env.DEEPL_API_KEY
+        })
+        .then(result => {
+            message.say(result.data.translations[0].text)
+        })
+        .catch(error => {
+            message.say(":x: Il semblerai qu'il y ai une erreur")
         })
     }
 }
