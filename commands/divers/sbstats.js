dbe1ce965d4019a37b5b0ddad21b4927cbb1f123
Wizi8914 <loick.konieczny@gmail.com>
2021-08-19 00:48:08 +0200

    fix bug

--- commands/divers/sbstats.js
+++ commands/divers/sbstats.js
@@ -21,25 +21,32 @@ module.exports = class SBCommand extends Command {
             message.say(":x: Vous devez citer le nom d'un joueur !")
         }
 
-        const { uuid } = await minecraftPlayer(args)
+        try {
+            var { uuid } = await minecraftPlayer(args)
+        } catch (error) {
+            return message.say(":x: Nous n'avon pas trouver votre pseudonime, Retaper le !")
+        }
+
         const { username } = await minecraftPlayer(uuid)
 
         const mcskin = `https://crafatar.com/renders/body/${uuid}?size=32&overlay`
         const url = `https://sky.shiiyu.moe/stats/${username}`
 
-        og(url, function(err, meta) {
-            const embed = new MessageEmbed()
-                .setTitle('Information de ' + meta.title)
-                .setURL(url)
-                .setColor('GREEN')
-                .setImage(mcskin)
-                .setDescription(meta.description)
-                .setFooter(botname, botimage)
-                .setTimestamp()
-
-            message.say(embed)
-        })
-
-        
+        try {
+            og(url, function(err, meta) {
+                const embed = new MessageEmbed()
+                    .setTitle('Information de ' + meta.title)
+                    .setURL(url)
+                    .setColor('GREEN')
+                    .setImage(mcskin)
+                    .setDescription(meta.description)
+                    .setFooter(botname, botimage)
+                    .setTimestamp()
+    
+                message.say(embed)
+            })
+        } catch (error) {
+            return message.say(":x: Nous n'avon pas trouver votre pseudonime ! retaper le")
+        }
     }
 }
