c983db2a2233bb800204b7dec77c8231d2cefc86
Wizi8914 <loick.konieczny@gmail.com>
2021-08-30 23:38:42 +0200

    fix bug

--- commands/utilitaire/botinfo.js
+++ commands/utilitaire/botinfo.js
@@ -23,10 +23,19 @@ module.exports = class BotInfoCommand extends Command {
         let totalMembers = 0
         const client = this.client
 
+        let commandlist = [];
+        let catelist = 0
+        for (let i = 2; i < this.client.registry.groups.size; i++) {
+            catelist++
+            for (var index = 0; index < this.client.registry.groups.toJSON()[i].commands.length; index++) {
+                commandlist.push(this.client.registry.groups.toJSON()[i].commands[index])
+            }
+        }
+
         for (const guild of this.client.guilds.cache) {
             totalMembers += (await guild[1].members.fetch()).size
         }
-
+        
         cpuStat.usagePercent(function(err, percent, seconds) {
 
             const embed = new MessageEmbed()
@@ -36,58 +45,34 @@ module.exports = class BotInfoCommand extends Command {
                 `Information de ${botname}`,
                 client.user.displayAvatarURL()
             )
-            .addFields(
-                {
-                    name: 'Tag du bot',
-                    value: client.user.tag,
-                },
-                {
-                    name: 'Version',
-                    value: botversion,
-                },
-                {
-                    name: 'Développeur',
-                    value: `WIZI#3492`,
-                },
-                {
-                    name: "Préfix du bot",
-                    value: message.guild.commandPrefix,
-                },
-                {
-                    name: 'Dernier restart',
-                    value: `${process.uptime().toFixed(2)}s`,
-                },
-                {
-                    name: 'Nombre de serveur',
-                    value: client.guilds.cache.size
-                },
-                {
-                    name: 'Membres Total',
-                    value: totalMembers,
-                },
-                {
-                    name: 'Repository',
-                    value: repository.url.replace('.git', ' ')
-                },
-                {
-                    name: '% du CPU',
-                    value: percent + '%'
-                },
-                {
-                    name: 'Version de Node.js',
-                    value: process.version
-                },
-                {
-                    name: 'Version de Discord.js',
-                    value: version
-                }
-            )
-            /*embed.addField("Links", [
-                ":star: [GitHub Repository](https://github.com/ravener/miyako)",
-                ":robot: [Upvote me at top.gg](https://top.gg/bot/397796982120382464)",
-                ":envelope_with_arrow: [Invite me to your server](https://discordapp.com/oauth2/authorize?client_id=397796982120382464&permissions=2016537702&scope=bot)",
-                ":video_game: [Join our Discord Server](https://discord.gg/mDkMbEh)"
-              ].join("\n")) */
+            .addField("Information du bot:", [
+                `:hash: **Tag du bot:** ${client.user.tag}`,
+                `:gear: **Version du bot :** ${botversion}`,
+                `:pushpin: **Préfix du bot :** ${message.guild.commandPrefix}`,
+                `<:discorddeveloperbadge:882009291907801089> **Développeur :** WIZI#3492`
+            ].join("\n"))
+            .addField("Information Serveurs:", [
+                `:notepad_spiral: **Nombre de server :** ${client.guilds.cache.size}`,
+                `:abacus: **Membres au total :** ${totalMembers}`
+            ].join("\n"))
+            .addField("Stats des Commands:", [
+                `:bookmark_tabs: **Nombre de Command :** ${commandlist.length}`,
+                `:clipboard: **Nombre de catégories :** ${catelist}`
+              ].join("\n"))
+            .addField("Versions:", [
+                `<:nodejs:881986645480591410> **Version de Node.js :** ${process.version}`,
+                `<:discordjs:881987374802935908> **Version de Discord.js :** v${version}`
+              ].join("\n"))
+            .addField('Statistique machine:', [
+                `:chart_with_downwards_trend: **Total De Memoir utiliser :** ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB`,
+                `:bar_chart: **Memoire Utiliser :** ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`,
+                `:chart_with_upwards_trend: **Pourcentage du CPU :** ${percent}%`,
+                `:arrows_counterclockwise: **Dernier restart :** ${process.uptime().toFixed(2)}s`
+            ].join("\n"))
+            .addField("Liens:", [
+                `<:github:881639629529444403> [Repository Github](${repository.url.replace('.git', ' ')})`,
+                ":globe_with_meridians: [Site Web](https://amaterasu-dev.netlify.app/)"
+            ].join("\n"))
 
             embed.setTimestamp()
 
